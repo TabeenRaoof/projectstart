0e3071bdd79f5b0b43b6d9000c53ac9a8810219b
TabeenRaoof <71104701+TabeenRaoof@users.noreply.github.com>
2022-03-30 08:26:22 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
-from cs50 import get_string
 
 # Prompt user for their name
 name = get_string("What is your name? ")
 
 # Print the name entered by user
-print(f"hello, {name}")
+print(f"Hello, {name}")
